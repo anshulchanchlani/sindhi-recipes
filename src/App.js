b676b32281c4d4a1a188f2b2fe4baa6036607b6c
anchanch <anchanch@microsoft.com>
2020-11-03 17:01:01 +0100

    Testing the workflow

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+
+  return <div>Welcome to Sindhi Recipes</div>;
 }
 
 export default App;
